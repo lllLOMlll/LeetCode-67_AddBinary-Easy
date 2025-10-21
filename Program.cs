a4b939817ad817855883ae717f1c207c99b76b76
Louis-Olivier Major <louis.olivier.major@gmail.com>
2025-10-21 08:19:50 -0400

    A version with switch case instead of if

--- Program.cs
+++ Program.cs
@@ -1,101 +1,107 @@
 ﻿using System.Collections;
+using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 
 public class Solution
 {
 
-    // public string fillingWithZero(string a, string b)
-    // {
-    //     string result = "";
-    //     string zeros = "";
-    //     int differenceLenth = Math.Abs(a.Length - b.Length);
-
-    //     if (a.Length < b.Length)
-    //     {
-    //         for (int k = 0; k < differenceLenth; k++)
-    //         {
-    //             zeros += "0";
-    //         }
-    //         result = zeros + a;
-    //     }
-    //     else
-    //     {
-    //         for (int k = 0; k < differenceLenth; k++)
-    //         {
-    //             zeros += "0";
-    //         }
-    //         result = zeros + b;
-    //     }
-
-    //     return result;
-    // }
-
     public string AddBinary(string a, string b)
     {
-
         int retainedAddition = 0;
         string result = "";
-        int longerLength = Math.Max(a.Length, b.Length);
-        int differenceLenth = Math.Abs(a.Length - b.Length);
+        int maxLength = Math.Max(a.Length, b.Length);
+        int sum = 0;
 
+        a = a.PadLeft(maxLength, '0');
+        b = b.PadLeft(maxLength, '0');
 
-        if (differenceLenth != 0)
+        for (int i = maxLength - 1; i >= 0; i--)
         {
-            if (a.Length > b.Length)
-            {
-                b = b.PadLeft(a.Length, '0');
-            }
-            else
-            {
-                a = a.PadLeft(b.Length, '0');
-            }
-        }
+            sum = (a[i] - '0') + (b[i] - '0');
 
-        for (int i = longerLength - 1; i >= 0; i--)
-        {
-
-            if (a[i] == '1' && b[i] == '1')
+            switch (sum)
             {
-                if (retainedAddition > 0)
-                {
-                    result += "1";
-                    retainedAddition--;
-                }
-                else
-                {
-                    result += "0";
-                }
-                retainedAddition++;
+                case 0:
+                    if (retainedAddition > 0)
+                    {
+                        result += "1";
+                        retainedAddition--;
+                    }
+                    else
+                    {
+                        result += "0";
+                    }
+                    break;
+                case 1:
+                    if (retainedAddition > 0)
+                    {
+                        result += "0";
+                        //retainedAddition--;
+                    }
+                    else
+                    {
+                        result += "1";
+                    }
+                    break;
+                case 2:
+                    if (retainedAddition > 0)
+                    {
+                        result += "1";                    
+                        break;
+                    }
+                    else
+                    {
+                        result += "0";
+                        retainedAddition++;
+                        break;
+                    }
+
             }
 
-            // Case 0 et 0 
-            if (a[i] == '0' && b[i] == '0')
-            {
-                if (retainedAddition > 0)
-                {
-                    result += "1";
-                    retainedAddition--;
-                }
-                else
-                {
-                    result += "0";
-                }
 
-            }
 
-            // Case 1 et 0 ou 0 et 1
-            if ((a[i] == '1' && b[i] == '0') || (a[i] == '0' && b[i] == '1'))
-            {
-                if (retainedAddition > 0)
-                {
-                    result += "0";
-                }
-                else
-                {
-                    result += "1";
-                }
-            }
+            // if (a[i] == '1' && b[i] == '1')
+            // {
+            //     if (retainedAddition > 0)
+            //     {
+            //         result += "1";
+            //         retainedAddition--;
+            //     }
+            //     else
+            //     {
+            //         result += "0";
+            //     }
+            //     retainedAddition++;
+            // }
+
+            // // Case 0 et 0 
+            // if (a[i] == '0' && b[i] == '0')
+            // {
+            //     if (retainedAddition > 0)
+            //     {
+            //         result += "1";
+            //         retainedAddition--;
+            //     }
+            //     else
+            //     {
+            //         result += "0";
+            //     }
+
+            // }
+
+            // // Case 1 et 0 ou 0 et 1
+            // if ((a[i] == '1' && b[i] == '0') || (a[i] == '0' && b[i] == '1'))
+            // {
+            //     if (retainedAddition > 0)
+            //     {
+            //         result += "0";
+            //     }
+            //     else
+            //     {
+            //         result += "1";
+            //     }
+            // }
 
             if (i == 0 && retainedAddition > 0)
             {
@@ -103,7 +109,6 @@ public class Solution
             }
 
         }
-
 
         // Inversing the result
         string reservedResult = new string(result.Reverse().ToArray());
@@ -113,41 +118,41 @@ public class Solution
 
 
     static void Main(string[] args)
-{
-    Solution s = new Solution();
+    {
+        Solution s = new Solution();
 
-    // string a1 = "11";
-    // string b1 = "1";
-    // string output1 = "100";
-    // Console.WriteLine(s.AddBinary(a1, b1));
-    // Console.WriteLine(output1);
+        string a1 = "11";
+        string b1 = "1";
+        string output1 = "100";
+        Console.WriteLine(s.AddBinary(a1, b1));
+        Console.WriteLine(output1);
 
-    // string a1_1 = "1";
-    // string b1_1 = "11";
-    // string output1_1 = "100";
-    // Console.WriteLine(s.AddBinary(a1_1, b1_1));
-    // Console.WriteLine(output1_1);
+        string a1_1 = "1";
+        string b1_1 = "11";
+        string output1_1 = "100";
+        Console.WriteLine(s.AddBinary(a1_1, b1_1));
+        Console.WriteLine(output1_1);
 
-    // string a2 = "1010";
-    // string b2 = "1011";
-    // string output2 = "10101";
-    // Console.WriteLine(s.AddBinary(a2, b2));
-    // Console.WriteLine(output2);
+        string a2 = "1010";
+        string b2 = "1011";
+        string output2 = "10101";
+        Console.WriteLine(s.AddBinary(a2, b2));
+        Console.WriteLine(output2);
 
-    // string a3 = "1011";
-    // string b3 = "1011";
-    // string output3 = "10110";
-    // Console.WriteLine(s.AddBinary(a3, b3));
-    // Console.WriteLine(output3);
+        string a3 = "1011";
+        string b3 = "1011";
+        string output3 = "10110";
+        Console.WriteLine(s.AddBinary(a3, b3));
+        Console.WriteLine(output3);
 
-    string a4 = "100";
-    string b4 = "110010";
-    string output4 = "110110";
-    Console.WriteLine(s.AddBinary(a4, b4));
-    Console.WriteLine(output4);
+        string a4 = "100";
+        string b4 = "110010";
+        string output4 = "110110";
+        Console.WriteLine(s.AddBinary(a4, b4));
+        Console.WriteLine(output4);
 
 
-}
+    }
 
 
 
--- obj/Debug/net9.0/67_AddBinary.AssemblyInfo.cs
+++ obj/Debug/net9.0/67_AddBinary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("67_AddBinary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8eb433127219ab3489ca2d97f22ab7907085204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8149774b3b5130e717fa62a91b991527d290a6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("67_AddBinary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("67_AddBinary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
